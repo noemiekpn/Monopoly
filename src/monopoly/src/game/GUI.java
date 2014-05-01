11dcb93eaee1f3c8141577ca90d81469881d4cc7
gbarros <gbbabarros@gmail.com>
2014-04-30 23:10:42 -0300

    MultiPanes

--- src/monopoly/src/game/GUI.java
+++ src/monopoly/src/game/GUI.java
@@ -5,12 +5,15 @@ package monopoly.src.game;
 //import java.io.IOException;
 
 //import javax.imageio.ImageIO;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Toolkit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 /**
@@ -21,6 +24,7 @@ import javax.swing.JPanel;
 public class GUI extends JFrame{
 	private static final long serialVersionUID = 1L;
 	JButton test = new JButton("Oh!");
+	JLayeredPane baseLayer = new JLayeredPane();
 	GUI_PANEL mainPanel= new GUI_PANEL();
 	
 	public GUI(String screenBarName,int width, int height) {
@@ -31,11 +35,33 @@ public class GUI extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle(screenBarName);
 		setVisible(true);
-		mainPanel.add(test);
-		getContentPane().add(mainPanel);
+		getContentPane().add(baseLayer, BorderLayout.CENTER);
 		
 	}
-
+	
+	public void loadBoard() {
+			
+		/*Carrega o Panel do Board*/
+		mainPanel.setBackground(Color.darkGray);
+		mainPanel.add(test);
+		mainPanel.setBounds(0, 0, 1016, 400);
+		mainPanel.setOpaque(true);
+		baseLayer.add(mainPanel,0);
+		//getContentPane().add(mainPanel,basicLayout.SOUTH,0);
+		
+	}
+	public void hudOptions() {
+		GUI_PANEL outro = new GUI_PANEL();
+		outro.setBackground(Color.BLUE);
+		outro.setBounds(0, 648,1016, 120);
+		outro.setOpaque(true);
+		baseLayer.add(outro,1);
+		
+	}
+	public void unLoadBoard() {
+		getContentPane().remove(0);
+		
+	}
 
 }
  class GUI_PANEL extends JPanel{
--- src/monopoly/src/game/Game.java
+++ src/monopoly/src/game/Game.java
@@ -3,6 +3,8 @@ package monopoly.src.game;
 public class Game {
 	public static void main(String args[]) {
 		GUI mainSreen = new GUI("Minha Tela",1024,768);
+		mainSreen.loadBoard();
 		System.out.print("oi");
+		mainSreen.hudOptions();
 	}
 }
