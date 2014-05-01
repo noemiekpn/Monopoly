7c1cb08e0c0617801f89fa53b627d3d795bfea71
gbarros <gbbabarros@gmail.com>
2014-04-30 21:27:33 -0300

    First change

--- src/monopoly/src/game/GUI.java
+++ src/monopoly/src/game/GUI.java
@@ -1,11 +1,12 @@
 package monopoly.src.game;
-
+import java.awt.image.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * This class implements all methods related to the Graphic User Interface.
@@ -13,20 +14,20 @@ import javax.swing.JFrame;
  */
 
 public class GUI extends JFrame{
-	BufferedImage dice1;
-	BufferedImage dice2;
 	
+	private static final long serialVersionUID = 1L;
+
 	public GUI() {
 		
 	}
+
+
+}
+ class GUI_PANEL extends JPanel{
+ private static final long serialVersionUID = 1L;
+ 
+ public void paintComponent(){
+	 
+ }
 	
-	private void loadImages() {
-	}
-	
-	public void displayDiceFace(int number) {
-		//drawImage(Image img, int dx1, int dy1, int dx2, int dy2, 
-		//int sx1, int sy1, int sx2, int sy2, ImageObserver observer)
-		
-		
-	}
 }
