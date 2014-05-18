bd6d22e2dc21403593e47061dc2a45d8aba738c2
gbarros <gbbabarros@gmail.com>
2014-05-18 19:42:12 -0300

    CardLoader almost done

--- src/monopoly/src/game/CardLoader.java
+++ src/monopoly/src/game/CardLoader.java
@@ -5,10 +5,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 import javax.imageio.ImageIO;
+
+import src.main.java.org.apache.commons.csv.CSVFormat;
+import src.main.java.org.apache.commons.csv.CSVFormat.CSVFormatBuilder;
+import src.main.java.org.apache.commons.csv.CSVRecord;
 
 public class CardLoader {
 	public Image dice1 = null;
@@ -18,251 +24,102 @@ public class CardLoader {
 	public Image[] companyCards = null;
 	public Image[] terrainCards = null;
 	public Image[] pins = null;
-	
-	private static int chanceSize;
-	private static int companySize;
-	private static int terrainSize;
-	private static int pinSize;
-	
-	private static String[] chanceImagePaths;
-	private static String[] companyImagePaths;
-	private static String[] terrainImagePaths;
-	private static String[] pinImagePaths;
-	
-	private static Chance chance[];
-	private static Company company[];
-	private static Terrain terrain[];
-	 class Chance{
-		public String imagePath;
-		public int value;
-		public char special;
-		public void setP(String a){
-			imagePath=a;
-		}
-	}
-	
-	class Company {
-		public String imagePath;
-		public int multiplier;
-		public int mortgage;
-		
-	}
-	
-	class Terrain{
-		public String imagePath;
-		public int rent;
-		public int house[]= new int [4];
-		public int hotel;
-		public int houseCost;
-		public int hotelCost;
-		public int mortgage;
-		
-	}
-	
-	private static void readFile(File dataFile){
-		
-		FileInputStream f = null;
-		BufferedReader reader = null;
-		
-		try{
-			f = new FileInputStream(dataFile);
-			reader = new BufferedReader(new InputStreamReader(f));
-			
-			String line = reader.readLine();
-            while(line != null){
-            	// Setting arrays sizes
-                if(line.compareTo("# NUMBER OF CHANCE CARDS") == 0) {
-                	line = reader.readLine();
-                	chanceSize = Integer.parseInt(line);
-          
-                }
-                
-                if(line.compareTo("# NUMBER OF COMPANY CARDS") == 0) {
-                	line = reader.readLine();
-                	companySize = Integer.parseInt(line);
-                	company = new Company[companySize];
-                	/*System.out.println(companySize);*/
-                }
 
-                if(line.compareTo("# NUMBER OF TERRAIN CARDS") == 0) {
-                	line = reader.readLine();
-                	terrainSize = Integer.parseInt(line);
-                	/*System.out.println(terrainSize);*/
-                }
-                
-                if(line.compareTo("# NUMBER OF PINS") == 0) {
-                	line = reader.readLine();
-                	pinSize = Integer.parseInt(line);
-                	/*System.out.println(pinSize);*/
-                }
-                
-                // Populating arrays
-                if(line.compareTo("# PATH OF EACH CHANCE CARD") == 0) {
-                	line = reader.readLine();
-                	String mainFolders = line;
-                	chanceImagePaths = new String[chanceSize];
-                	chance = new Chance[chanceSize];
-                	line = reader.readLine();
-                	String parts[];
-                	for(int i = 0; i < chanceSize; i++){
-                		chanceImagePaths[i] = mainFolders+line;
-                		System.out.println(chanceImagePaths[i]);
-                		chance[i].setP(mainFolders+line) ; //Nessa Linha
-                		System.out.println(chance[i].imagePath);
-                		line = reader.readLine();
-                		parts = line.split(" ");
-                		if (parts[0].compareTo("+")==0){
-                			chance[i].value= Integer.parseInt(parts[1]);
-                			chance[i].special ='A';
-                		}
-                		else if(parts[0].compareTo("-")==0){
-                			chance[i].value= Integer.parseInt(parts[1]);
-                			chance[i].special ='S';
-                		}
-                		else if (parts[0].compareTo("*")==0){
-                			chance[i].special='M';
-                			chance[i].value= Integer.parseInt(parts[1]);
-                		}
-                		else if (parts[0].compareTo("P")==0){
-                			chance[i].special='P';
-                			chance[i].value=0;
-                		}
-                		else if (parts[0].compareTo("L")==0){
-                			chance[i].special='L';
-                			chance[i].value=0;
-                		}
-                	}
-                	
-                	for(int i = 0; i < chanceSize; i++){
-                		System.out.println(chanceImagePaths[i]);
-                	}
-                }
-                
-                if(line.compareTo("# PATH OF EACH COMPANY CARD") == 0) {
-                	line = reader.readLine();
-                	String mainFolders = line;
-                	companyImagePaths= new String[companySize];
-              
-                	String parts[];
-                	line = reader.readLine();
-                	for(int i = 0; i < companySize; i++){
-                		companyImagePaths[i] = mainFolders + line;
-                		company[i].imagePath=companyImagePaths[i];
-                		line = reader.readLine();
-                		parts= line.split(" ");
-                		company[i].multiplier= Integer.parseInt(parts[0]);
-                		company[i].mortgage= Integer.parseInt(parts[1]);
-                	}
-                	
-                	/*for(int i = 0; i < companySize; i++){
-                		System.out.println(companyImagePaths[i]);
-                	}*/
-                }
-                
-                if(line.compareTo("# PATH OF EACH TERRAIN CARD") == 0) {
-                	line = reader.readLine();
-                	String mainFolders = line;
-                	terrainImagePaths= new String[terrainSize];
-                	String parts[];
-                	line = reader.readLine();
-                	for(int i = 0; i < terrainSize; i++){
-                		terrainImagePaths[i] = mainFolders + line;
-                		terrain[i].imagePath= terrainImagePaths[i];
-                		line = reader.readLine();
-                		parts = line.split(" ");
-                		terrain[i].rent = Integer.parseInt(parts[0]);
-                		terrain[i].house[0] = Integer.parseInt(parts[1]);
-                		terrain[i].house[1] = Integer.parseInt(parts[2]);
-                		terrain[i].house[2] = Integer.parseInt(parts[3]);
-                		terrain[i].house[3] = Integer.parseInt(parts[4]);
-                		terrain[i].hotel = Integer.parseInt(parts[5]);
-                		terrain[i].houseCost = Integer.parseInt(parts[6]);
-                		terrain[i].houseCost = Integer.parseInt(parts[7]);
-                		terrain[i].mortgage = Integer.parseInt(parts[8]);
-                	}
-                	
-                	/*for(int i = 0; i < terrainSize; i++){
-                		System.out.println(terrainImagePaths[i]);
-                	}*/
-                }
-                
-                if(line.compareTo("# PATH OF EACH PIN") == 0) {
-                	line = reader.readLine();
-                	String mainFolders = line;
-                	pinImagePaths= new String[pinSize];
-                	
-                	line = reader.readLine();
-                	for(int i = 0; i < pinSize; i++){
-                		pinImagePaths[i] = mainFolders + line;
-                		line = reader.readLine();
-                	}
-                	
-                	/*for(int i = 0; i < pinSize; i++){
-                		System.out.println(pinImagePaths[i]);
-                	}*/
-                }
-                
-                line = reader.readLine();
-            } 
-
-			
-		} catch (FileNotFoundException e){
-			System.out.println("ERROR: File could not be found.");
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				reader.close();
-				f.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-		}
-		
-	}
 	
-	public void load(File dataFile) {
-		
+	
+	/*Novas variáveis para o novo parser*/
+	String paths[] = new String[4]; // 0 chance, 1 company, 2 terrain, 3 pin
+	String names[] = new String[4];
+	
+	public void load(String dataFile,Chance []cardChance,Property []properties,Pin []pins) {
+		Reader input;
 		try {
-			readFile(dataFile);
+			input = new FileReader (dataFile);
+			Iterable<CSVRecord> lines = CSVFormat.EXCEL.parse(input);
+			
+			int i=0;
+			for(CSVRecord line:lines){
+				paths[i]= new String();
+				names[i]= new String();
+				paths[i]=line.get(0);//paths
+				names[i]=line.get(1);//names
+				i++;
+				//System.out.println(paths[i]+names[i]);
+			}
+			/*Now opens each file to set it's objects*/
+			
+			System.out.println(paths[0]+names[0]);
+			input= new FileReader (paths[0]+names[0]); //CHANCE
+			lines = CSVFormat.EXCEL.parse(input);
+			i=0;
+			for(CSVRecord line:lines){
+				cardChance[i] = new Chance(
+						line.get(1).charAt(0),//"function"
+						Integer.parseInt(line.get(2)),//"value"
+						ImageIO.read(new File(paths[0]+line.get(0)))//"name"
+						);
+				i++;
+			}
+			
+			input= new FileReader (paths[1]+names[1]);//Company
+			lines = CSVFormat.EXCEL.parse(input);
+			i=0;
+			for(CSVRecord line:lines){
+				System.out.println(paths[1]+line.get(0));
+				properties[i] =(Property) new Company(
+						line.get(0),//"name"
+						Integer.parseInt(line.get(1)),//"price"
+						Integer.parseInt(line.get(2)),//"multiplier"
+						Integer.parseInt(line.get(3)),//"mortgage"
+						Integer.parseInt(line.get(4)),//"location"
+						ImageIO.read(new File(paths[1]+line.get(0)))//"name"
+						);
+				i++;
 
-			// Load single images
-			dice1 = ImageIO.read(new File("src/res/dice_sheet.png"));
-			dice2 = ImageIO.read(new File("src/res/dice_sheet.png"));
-			board = ImageIO.read(new File("src/res/table.png"));
-			
-			// Load arrays of images
-			chanceCards = new Image[chanceSize];
-			companyCards = new Image[companySize];
-			terrainCards = new Image[terrainSize];
-			pins = new Image[pinSize];
-			
-			for(int i = 0; i < chanceSize; i++) {
-				chanceCards[i] = ImageIO.read(new File(chanceImagePaths[i]));
 			}
 			
-			for(int i = 0; i < companySize; i++) {
-				companyCards[i] = ImageIO.read(new File(companyImagePaths[i]));
+			input= new FileReader (paths[2]+names[2]);//terrain
+			lines = CSVFormat.EXCEL.parse(input);
+			//i=0;
+			for(CSVRecord line:lines){
+				properties[i] = (Property) new Terrain(
+						line.get(0),//"name"
+						Integer.parseInt(line.get(1)),//"price"
+						Integer.parseInt(line.get(2)),//"rent"
+						Integer.parseInt(line.get(3)),//"mortgage"
+						Integer.parseInt(line.get(4)),//"location"
+						ImageIO.read(new File(paths[2]+line.get(0))),//"name"
+						new Integer[] {
+							Integer.parseInt(line.get(5)),//"house1"
+							Integer.parseInt(line.get(6)),//"house2"
+							Integer.parseInt(line.get(7)),//"house3"
+							Integer.parseInt(line.get(8)),//"house4"
+							Integer.parseInt(line.get(9))//"hotel"
+							},
+							Integer.parseInt(line.get(10))//"construction"
+						);
+				i++;
 			}
 			
-			for(int i = 0; i < terrainSize; i++) {
-				terrainCards[i] = ImageIO.read(new File(terrainImagePaths[i]));
+			input= new FileReader (paths[3]+names[3]);//pin
+			lines = CSVFormat.EXCEL.parse(input);
+			i=0;
+			for(CSVRecord line:lines){
+				pins[i] = new Pin(
+						line.get("color"),
+						ImageIO.read(new File(paths[3]+line.get("name")))
+						);
+				i++;
+
 			}
 			
-			for(int i = 0; i < pinSize; i++) {
-				pins[i] = ImageIO.read(new File(pinImagePaths[i]));
-			}
 			
-		} catch (IOException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	/*public static final void main(String[] args) {
-		ImageLoader i = new ImageLoader();
-		File dataFile = new File("src/res/data.config");
-		
-		i.load(dataFile);
-	}*/
+	    catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	    }
+}
 }
